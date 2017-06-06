1bd98056a7f23e15dacb2c44f8a6ffe44f78967a
Athenie Shi <athenieshi@gmail.com>
2017-06-06 15:24:45 -0400

    use createElement and appendChild method

--- index.js
+++ index.js
@@ -14,17 +14,32 @@ function handleSubmit(ev){
 
     // const boldedName = document.createElement('strong')
     // boldedName.textContent = name 
-
     // details.appendChild(boldedName)
+
     // console.log(f.personName.value + f.personSchool.value)
 
-details.innerHTML = `
-<ul>
-    <li>Name: ${name}</li>
-    <li>Favorite Color: ${colorDiv}</li>
-    <li>Age: ${age}</li>
-</ul>
-`
+// details.innerHTML = `
+// <ul>
+//     <li>Name: ${name}</li>
+//     <li>Favorite Color: ${colorDiv}</li>
+//     <li>Age: ${age}</li>
+// </ul>
+// `
+
+const liName = document.createElement('li')
+const liFavoriteColor = document.createElement('li')
+const liAge = document.createElement('li')
+const ulList = document.createElement('ul')
+
+liName.textContent = name
+liFavoriteColor.innerHTML = colorDiv
+liAge.textContent = age
+
+ulList.appendChild(liName)
+ulList.appendChild(liFavoriteColor)
+ulList.appendChild(liAge)
+
+details.appendChild(ulList)
         }
 
 personForm.addEventListener('submit', handleSubmit)
