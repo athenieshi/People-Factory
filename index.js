95108f83d3e9d7ce3257fc35fe30ae33781dc410
Athenie Shi <athenieshi@gmail.com>
2017-06-06 10:50:33 -0400

    Add paragraph

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ function handleSubmit(ev){
     const heading = document.querySelector('h1')
     heading.textContent = f.personName.value + " goes to " + f.personSchool.value
     console.log(f.personName.value + f.personSchool.value)
-}
+    const paragraph = document.querySelector('#location')
+    paragraph.textContent = f.personSchool.value + " rocks"
+        }
 
 personForm.addEventListener('submit', handleSubmit)
 
