97d5e18d914fb7ba2f9e097e28bbaa2e9f7dccfb
Athenie Shi <athenieshi@gmail.com>
2017-06-06 10:40:56 -0400

    combine header

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const personForm = document.querySelector('#personForm')
 
 function handleSubmit(ev){
+    console.log(ev)
     ev.preventDefault()
     const f = ev.target
     const heading = document.querySelector('h1')
-    heading.textContent = f.personName.value
-    console.log(f.personName.value)
+    heading.textContent = f.personName.value + " goes to " + f.personSchool.value
+    console.log(f.personName.value + f.personSchool.value)
 }
 
 personForm.addEventListener('submit', handleSubmit)
